a91b0173a9e4bdbac41b1523a48b666210bca30c
zouigmohamed <107324328+zouigmohamed@users.noreply.github.com>
2024-01-10 11:18:06 +0000

    fix error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,24 +4,7 @@ const prisma = new PrismaClient();
 
 async function main() {
 
-  // const user = await prisma.user.createMany({
-  //   data: Array.from({ length: 25 }, () =>  ({
-  //     email: faker.internet.email(),
-  //     name: faker.internet.userName(),
-  //     address: {
-  //       city: faker.location.city(),
-  //       street: faker.location.street(),
-  //       state: faker.location.state(),
-  //       zip: faker.location.zipCode(),
-  //     },
-  //   }))
-  // });
-  const todo = await prisma.todo.create({
-    data: {
-      title: faker.lorem.text(),
-      body: faker.lorem.paragraph(),
-    }
-  });
+
 }
 
 main()
